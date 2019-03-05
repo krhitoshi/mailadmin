24d4867488b18a658f38a58b2182bcf44b6a0c08
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-03-05 22:13:58 +0900

    Add DomainsController

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,4 +1,6 @@
 class Domain < ApplicationRecord
   self.table_name = :domain
   self.primary_key = :domain
+
+  has_many :rel_mailboxes, class_name: "Mailbox", foreign_key: :domain
 end
--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -1,4 +1,6 @@
 class Mailbox < ApplicationRecord
   self.table_name = :mailbox
   self.primary_key = :username
+
+  belongs_to :rel_domain, class_name: "Domain", foreign_key: :domain
 end
