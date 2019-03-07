c47c09068a912f7ed1ee5f507fe5bae194f0dd8e
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-03-07 15:14:14 +0900

    Create a new mailbox

--- app/controllers/mailboxes_controller.rb
+++ app/controllers/mailboxes_controller.rb
@@ -1,5 +1,39 @@
 class MailboxesController < ApplicationController
   before_action :set_mailbox, only: [:edit, :update]
+  before_action :set_domain, only: [:new]
+
+  def new
+    @mailbox = Mailbox.new
+    @mailbox.rel_domain = @domain
+  end
+
+  def create
+    params = mailbox_params.dup
+
+    if params["password"] != params["password_confirmation"]
+      params.delete("password_confirmation")
+      @mailbox = Mailbox.new(params)
+      flash[:notice] = "Password mismatch"
+      render action: 'new'
+      return
+    end
+
+    params["password"] = DovecotCrammd5.calc(params["password"])
+    params.delete("password_confirmation")
+    @mailbox = Mailbox.new(params)
+
+    # TODO: Validation of username
+    local_part, _ = @mailbox.username.split("@")
+    @mailbox.maildir = "#{@mailbox.domain}/#{@mailbox.username}/"
+    @mailbox.local_part  = local_part
+    @mailbox.build_alias(goto: @mailbox.username, domain: @mailbox.domain)
+
+    if @mailbox.save
+      redirect_to domain_path(@mailbox.domain), notice: 'Mailbox was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
 
   def edit
   end
@@ -25,11 +59,16 @@ class MailboxesController < ApplicationController
   end
 
   private
+
+  def set_domain
+    @domain = Domain.find(params[:domain_id])
+  end
+
   def set_mailbox
     @mailbox = Mailbox.find(params[:id])
   end
 
   def mailbox_params
-    params.require(:mailbox).permit(:name, :password, :password_confirmation, :quota, :active)
+    params.require(:mailbox).permit(:username, :name, :domain, :password, :password_confirmation, :quota, :active)
   end
 end
