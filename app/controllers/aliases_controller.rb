1454f619d03dde1e4ee8d6db32be73340640d3fe
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-03-07 14:44:34 +0900

    Create a new alias

--- app/controllers/aliases_controller.rb
+++ app/controllers/aliases_controller.rb
@@ -1,5 +1,21 @@
 class AliasesController < ApplicationController
   before_action :set_alias, only: [:edit, :update]
+  before_action :set_domain, only: [:new]
+
+  def new
+    @alias = Alias.new
+    @alias.rel_domain = @domain
+  end
+
+  def create
+    @alias = Alias.new(alias_params)
+
+    if @alias.save
+      redirect_to domain_path(@alias.domain), notice: 'Alias was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
 
   def edit
   end
@@ -13,11 +29,16 @@ class AliasesController < ApplicationController
   end
 
   private
+
+  def set_domain
+    @domain = Domain.find(params[:domain_id])
+  end
+
   def set_alias
     @alias = Alias.find(params[:id])
   end
 
   def alias_params
-    params.require(:alias).permit(:goto, :active)
+    params.require(:alias).permit(:address, :domain, :goto, :active)
   end
 end
