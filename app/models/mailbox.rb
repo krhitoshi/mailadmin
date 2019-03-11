d3a01fc8c196f112cfdd40216a8006097d942911
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-03-11 13:47:02 +0900

    Mailbox.username cannot be changed on update

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -2,8 +2,15 @@ class Mailbox < ApplicationRecord
   self.table_name = :mailbox
   self.primary_key = :username
 
+  include DovecotCramMD5Password
+
   belongs_to :rel_domain, class_name: "Domain", foreign_key: :domain
   has_one :alias, foreign_key: :address, dependent: :destroy
+
+  # just in case
+  validate on: :update do |mailbox|
+    mailbox.errors.add(:username, 'cannot be changed') if mailbox.username_changed?
+  end
 
   def quota_str
     if quota.zero?
