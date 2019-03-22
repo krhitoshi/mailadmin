2f59383393c185b186718dde3d062ee466e6d476
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-03-22 18:56:08 +0900

    Remove jQuery codes

--- app/assets/javascripts/aliases.js
+++ app/assets/javascripts/aliases.js
@@ -1,6 +1,6 @@
 (function() {
     function deleteAddress() {
-        $(this).closest(".input-group").remove();
+        this.parentNode.parentNode.remove();
     }
 
     document.addEventListener("turbolinks:load", function () {
@@ -10,13 +10,13 @@
         }
         addButton.addEventListener('click', function () {
             var address = document.querySelector(".forward-address");
-            var newAddress = $(address).clone();
-            newAddress.children("input").val("");
-            var button = newAddress.find(".delete-forward-address-button");
-            button.removeAttr("disabled");
-            button.click(deleteAddress);
-            newAddress.children(".input-group-append").removeClass("d-none");
-            newAddress.insertBefore(addButton);
+            var newAddress = address.cloneNode(true);
+            newAddress.querySelector("input").value = "";
+            var button = newAddress.querySelector(".delete-forward-address-button");
+            button.removeAttribute("disabled");
+            button.addEventListener('click', deleteAddress);
+            newAddress.querySelector(".input-group-append").classList.remove("d-none");
+            addButton.parentNode.insertBefore(newAddress, addButton);
         });
 
         document.querySelectorAll(".delete-forward-address-button").forEach(function (button){
